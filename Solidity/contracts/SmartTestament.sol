dc3a96836c1055e98417faa28b17a677247b0074
EliseyBelov <elisey.belov777@gmail.com>
2023-05-26 19:48:30 +0300

    ...

--- Solidity/contracts/SmartTestament.sol
+++ Solidity/contracts/SmartTestament.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 /**
